d8dbda52ac5a4064475d7e73170dd160229d14f7
Joe,Chen <Joe@med.grt.bz>
2011-10-04 17:30:09 +0800

    IM

---
+++ openfire/src/java/StandaloneStarter.java
@@ -0,0 +1,12 @@
+import org.jivesoftware.openfire.XMPPServer;
+
+
+public class StandaloneStarter {
+
+	public static void main(String[] args) {
+
+	      XMPPServer server = new XMPPServer();
+
+	    }
+	
+}
